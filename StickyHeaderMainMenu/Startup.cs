35d0f0aa976a4f7ac787450dfa7f03f4adc2d12a
vasuvoy <vasuvoy@gmail.com>
2021-02-11 10:04:54 +0530

    first commit

--- StickyHeaderMainMenu/Startup.cs
+++ StickyHeaderMainMenu/Startup.cs
@@ -63,6 +63,7 @@ namespace StickyHeaderMainMenu
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseDefaultFiles();
             if (!env.IsDevelopment())
             {
                 app.UseSpaStaticFiles();
